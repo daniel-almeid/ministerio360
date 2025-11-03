d0df15f3229df3d128a5d53c9e29594f5e828fc0
Daniel Andrade <daniel.andrade@floripabit.com.br>
2025-11-03 19:33:56 -0300

    update taiwilnd

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
-  experimental: {
-    turbo: {
-      rules: {
-        // Garante que CSS usa PostCSS normal (Tailwind)
-        '*.css': ['postcss-loader'],
-      },
-    },
-    // 🚫 Desativa Lightning CSS para evitar erro de build na Vercel
-    optimizeCss: false,
-    lightningcss: false,
-  },
+const nextConfig: NextConfig = {
+
 };
 
-module.exports = nextConfig;
+export default nextConfig;
